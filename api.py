00c1b7d16c42b2827e4eabf96611b0ed4936be07
Tolga <tolgatasci1@gmail.com>
2018-12-20 19:11:45 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,23 +1,24 @@
 import requests
 import json
 import os.path
-from helper import *
+from helper import helper
 
 class api():
     api_url = "https://api2.musical.ly/"
     global_veriable = {}
     active_user = {}
+    helper = helper()
     def __init__(self):
         var = 1
     def login(self,username,password):
-        username = helper.xor(username)
+        username = self.helper.xor(str=username)
         if os.path.exists(username+'.json'):
             with open(username+'.json', encoding='utf-8') as json_file:
                 load = json.load(json_file)
                 self.active_user = load
                 return load
-        password = helper.xor(password)
-        url = self.api_url+"passport/user/login/?"+helper.query(helper.default_veriable(self.global_veriable))
+        password = self.helper.xor(password)
+        url = self.api_url+"passport/user/login/?"+self.helper.query(self.helper.default_veriable(self.global_veriable))
 
         posts = {
             'mix_mode':1,
@@ -28,7 +29,7 @@ class api():
             'account': None,
             'captcha': None
         }
-        login = helper.request_post(url,posts)
+        login = self.helper.request_post(url,posts)
 
         try:
             headers = {}
@@ -43,7 +44,7 @@ class api():
         else:
             data = login.json()
             if (data.get('data') and (data.get('error_code') == None)):
-                success['data'] = helper.user_data_export(data.get('data'))
+                success['data'] = self.helper.user_data_export(data.get('data'))
                 success['cookies'] = headers
                 with open(username +'.json', 'w') as outfile:
                     json.dump(success, outfile)
@@ -52,17 +53,26 @@ class api():
                 return login.json()
 
     def home_list(self,user_data = {}):
-        url = self.api_url + "aweme/v1/feed/?count=20&offset=0&max_cursor=0&type=0&is_cold_start=1&pull_type=1&"+helper.query(helper.default_veriable())
+        url = self.api_url + "aweme/v1/feed/?count=20&offset=0&max_cursor=0&type=0&is_cold_start=1&pull_type=1&"+self.helper.query(self.helper.default_veriable())
         if(user_data.__len__()>0):
-            data = helper.request_get(self,url,session=self.active_user['cookies'])
+            data = self.helper.request_get(self,url,session=self.active_user['cookies'])
         else:
-            data = helper.request_get(self,url)
+            data = self.helper.request_get(self,url)
         return data.json()
     def search_user(self,text = 'teamtolga'):
-        url = self.api_url + "aweme/v1/discover/search/?cursor=0&keyword="+text+"&count=10&type=1&hot_search=0&"+helper.query(helper.default_veriable())
-        data = helper.request_get(self,url)
+        url = self.api_url + "aweme/v1/discover/search/?cursor=0&keyword="+text+"&count=10&type=1&hot_search=0&"+self.helper.query(self.helper.default_veriable())
+        data = self.helper.request_get(self,url)
         return data.json()
     def like_post(self,aweme_id = 1, type='1', session = {}):
-        url = self.api_url + "aweme/v1/commit/item/digg/?aweme_id="+ aweme_id +"&type="+ type +"&retry_type=no_retry&from=3&"+helper.query(helper.default_veriable())
-        data = helper.request_get(self,url,session=session)
+        url = self.api_url + "aweme/v1/commit/item/digg/?aweme_id="+ aweme_id +"&type="+ type +"&retry_type=no_retry&from=3&"+self.helper.query(self.helper.default_veriable())
+        data = self.helper.request_get(self,url,session=session)
+        return data.json()
+    def view_post(self,aweme_id = 1,session = {}):
+        data = self.helper.default_veriable()
+        data['aweme_type'] = 0
+        data['play_delta'] = 1
+        data['item_id'] = aweme_id
+        url = self.api_url + "aweme/v1/aweme/stats/?"+self.helper.query(self.helper.default_veriable())
+
+        data = self.helper.request_post(url,posts=data,session=session)
         return data.json()
