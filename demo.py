b4feb5207890ff73f977ae5118cc57c5d109a8ad
Tolga <tolgatasci1@gmail.com>
2018-12-20 15:56:16 +0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,8 +1,5 @@
 from api import api
 import sys
-import sqlite3
-
-db = sqlite3.connect('tiktok.db')
 if __name__ == "__main__":
     api = api()
     #api.global_veriable['costum_query_field'] = "value"
