9a8f4760999b2fbb681452725832c7fee146b98e
Tolga <tolgatasci1@gmail.com>
2018-12-20 18:10:52 +0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,5 +2,13 @@ from api import api
 import sys
 if __name__ == "__main__":
     api = api()
-    #api.global_veriable['costum_query_field'] = "value"
-    print (api.login("username","password"))
+    api.global_veriable['as'] = "test"
+    api.global_veriable['cp'] = "test"
+    (api.login("",""))
+
+    # Cookie Active User
+    #home_list = api.home_list(api.active_user)
+    # No Cookie
+    home_list = api.home_list()
+    print(home_list)
+
