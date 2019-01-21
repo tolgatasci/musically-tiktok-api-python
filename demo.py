08c3a9da34febddd73452f6ee4ff6367ae9a3bae
Tolga <tolgatasci1@gmail.com>
2019-01-21 19:05:10 +0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,22 +1,32 @@
 from api import api
+from time import sleep
 import sys
+import os
+
+
+def login_(login):
+    if (login.get('code')):
+        from capthca import capthca
+        cc = capthca(login.get('code'))
+        if (cc.backdata):
+            if (len(cc.backdata) > 0):
+                login = api.login(username, password, cc.backdata)
+                del cc
+                login_(login)
+        else:
+            print('empty form again')
+            login = api.login(username, password, cc.backdata)
+            del cc
+            login_(login)
+
 if __name__ == "__main__":
     api = api()
-    #api.global_veriable['as'] = "test" change Device Info
-    #api.global_veriable['cp'] = "test" change Device Info
-    (api.login("",""))
 
-    # Cookie Active User
-    #home_list = api.home_list(api.active_user)
-    # No Cookie
-    #home_list = api.home_list()
-    #print(home_list)
+    api.global_veriable['device_id'] = "6648944787888948741"
+    api.global_veriable['iid'] = "6648944787888948741"
+    api.global_veriable['openudid'] = "6vchx2vx3ubd051q"
+    username = ""
+    password = ""
+    login = api.login(username,password)
+    login_(login)
 
-    #search = api.search_user(text='teamtolga')
-    #print(search)
-
-
-    #like = api.like_post(aweme_id='6632541425961536773',type='1',session=api.active_user['cookies'])
-    #print(like)
-    #unlike = api.like_post(aweme_id='6632541425961536773',type='0',session=api.active_user['cookies'])
-    #print(unlike)
